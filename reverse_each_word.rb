37b849cdda419fef1e76e39d36e6dddfdcb08da7
Taylor Mills <taylormills24@yahoo.com>
2019-03-06 12:52:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,12 @@ reverse_each_word = ["Hello there,and how are you?"]
 
 def reverse(words)
  words.each do |word|
- puts "Reverse each #{word}!"
+ puts "Reverse each #{word} in the sentence!"
  end
 end
  
 def reverse(words)
  words.collect do |word|
- "Reverse each #{word}!"
+ "Reverse each #{word} in the sentence!"
  end
 end
